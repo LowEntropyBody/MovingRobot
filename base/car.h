3e3e2e5c70aed94e07b5c1e2a0f3e095166d77c0
LowEntropyBody <jszyyx@163.com>
2018-05-10 10:13:06 +0800

    主线程逻辑

--- base/car.h
+++ base/car.h
@@ -7,6 +7,10 @@
 #include <vector>
 using namespace std;
 
+class Car{
+	
+}
+
 //速度模式
 class CarSpeed{
 	public:
@@ -81,130 +85,6 @@ void CarSpeed::move_frist_start(){
 	write(usart_fd, ch, sizeof(ch));
 	tcflush(usart_fd, TCIFLUSH);//清空in缓冲区
     tcflush(usart_fd, TCOFLUSH);//清空out缓冲区
-}
-
-//位置模式
-class Car{
-	double now_degree;
-	public:
-		Car();
-		~Car();
-		void move_frist_start();
-		void move_rotate(double degree);
-		void move_forward(double distance);
-		
-};
-
-void Car::move_forward(double distance){
-	int degree_temp = now_degree * 21;
-	int distance_temp = distance * 63;
-	int high,low;
-	char ch[10] = {0xff,0xfe,2,0,0,20,0,20,0,0x07};
-	
-	if(degree_temp >= 0){
-		ch[9] = 0x07;
-		//A轮不动
-		low = degree_temp%256;
-		high = degree_temp/256;
-		ch[3] = high;
-		ch[4] = low;
-		//C轮正转
-		int distancec = degree_temp + distance_temp;
-		low = distancec%256;
-		high = distancec/256;
-		ch[7] = high;
-		ch[8] = low;
-		//B轮反转
-		int distanceb = degree_temp - distance_temp;
-		if(distanceb >= 0){
-			low = distanceb%256;
-			high = distanceb/256;
-			ch[5] = high;
-			ch[6] = low;
-		}else{
-			distanceb = -distanceb;
-			low = distanceb%256;
-			high = distanceb/256;
-			ch[5] = high;
-			ch[6] = low;
-			ch[9] = 0x05;
-		}
-	}else{
-		degree_temp = -degree_temp;
-		ch[9] = 0x00;
-		//A轮不动
-		low = degree_temp%256;
-		high = degree_temp/256;
-		ch[3] = high;
-		ch[4] = low;
-		
-		//B轮反转
-		int distanceb = degree_temp + distance_temp;
-		low = distanceb%256;
-		high = distanceb/256;
-		ch[5] = high;
-		ch[6] = low;
-		
-		//C轮正转
-		int distancec = degree_temp - distance_temp;
-		if(distancec >= 0){
-			low = distancec%256;
-			high = distancec/256;
-			ch[7] = high;
-			ch[8] = low;
-		}else{
-			distancec = -distancec;
-			low = distancec%256;
-			high = distancec/256;
-			ch[7] = high;
-			ch[8] = low;
-			ch[9] = 0x01;
-		}
-		
-	}	
-	write(usart_fd, ch, sizeof(ch));
-	tcflush(usart_fd, TCIFLUSH);//清空in缓冲区
-    tcflush(usart_fd, TCOFLUSH);//清空out缓冲区
-	
-}
-
-void Car::move_rotate(double degree){
-	now_degree = degree;
-	int degree_temp = degree * 21;
-	int high,low;
-	char ch[10] = {0xff,0xfe,2,20,0,20,0,20,0,0x07};
-	if(degree_temp >= 0){
-		low = degree_temp%256;
-		high = degree_temp/256;
-		ch[3] = ch[5] = ch[7] = high;
-		ch[4] = ch[6] = ch[8] = low;
-	}else{
-		degree_temp = -degree_temp;
-		low = degree_temp%256;
-		high = degree_temp/256;
-		ch[3] = ch[5] = ch[7] = high;
-		ch[4] = ch[6] = ch[8] = low;
-		ch[9] = 0x00;
-	}
-	write(usart_fd, ch, sizeof(ch));
-	tcflush(usart_fd, TCIFLUSH);//清空in缓冲区
-    tcflush(usart_fd, TCOFLUSH);//清空out缓冲区
-}
-
-void Car::move_frist_start(){
-	char ch[1] = {0x00};
-	int re = usart_init();
-	if(re < 0) exit(-1);
-	write(usart_fd, ch, sizeof(ch));
-	tcflush(usart_fd, TCIFLUSH);//清空in缓冲区
-    tcflush(usart_fd, TCOFLUSH);//清空out缓冲区
-}
-
-Car::Car(){
-	now_degree = 0;
-}
-Car::~Car(){
-	usart_close();
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,32 +14,8 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-	int xs,ys,zs;
-	{
-		stringstream ss;
-		ss << argv[1];
-		ss >> xs;
-	}
-	{
-		stringstream ss;
-		ss << argv[2];
-		ss >> ys;
-	}
-	{
-		stringstream ss;
-		ss << argv[3];
-		ss >> zs;
-	}
-	usleep(1000*1000*20);
-	CarSpeed* cs = new CarSpeed(); 
-	cs -> move_frist_start();
-	for(int i=0; i< 75; i++){
-		cs -> speed_x_y_z(xs, ys, zs);
-		usleep(1000*2000);
-		cs -> speed_x_y_z(xs, -ys, zs);
-		usleep(1000*2000);
-	}
-	cs -> speed_x_y_z(0, 0, 0);
+	cout << "----Main Thread Start----" << endl;
 	
+	cout << "----Main Thread End----" << endl;
 	return 0;
 }
