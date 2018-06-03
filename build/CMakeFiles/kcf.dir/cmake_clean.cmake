a210a963c1679f5a7e9119cd1d08bb3f257fabb1
wozhouh <hwz17@mails.tsinghua.edu.cn>
2018-06-03 22:19:12 +0800

    hwz

---
+++ build/CMakeFiles/kcf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kcf.dir/src/ImgTrackerKCF.cpp.o"
+  "CMakeFiles/kcf.dir/src/fhog.cpp.o"
+  "CMakeFiles/kcf.dir/src/mainKCF.cpp.o"
+  "kcf.pdb"
+  "kcf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kcf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
